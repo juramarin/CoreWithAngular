f9348c81c994f423bb1a7e8f7a1929fef3b35d9e
M.J <marin.juranko@outlook.com>
2018-11-21 11:29:04 +0100

    Add project files.

---
+++ CoreWithAngular/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreWithAngular.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
